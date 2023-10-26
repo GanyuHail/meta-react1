d076bb448415c4c3cf991cfe1504e87161b32cca
GanyuHail <103945576+GanyuHail@users.noreply.github.com>
2023-10-26 14:15:39 +0100

    rm parent folder react built into current folder

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return ( 
     <div className="App"> 
       This is the starting code for "Your first component" ungraded lab 
-      <Heading/> 
+      <Heading /> 
     </div> 
   ); 
 } 
